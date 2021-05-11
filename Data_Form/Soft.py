8160262c72afaab064e526a52d35a49d1a62cb66
epsyllon <bruno.moutounaick.e.c@gmail.com>
2021-05-11 11:35:47 +0200

    11/5  11:30

--- Data_Form/Soft.py
+++ Data_Form/Soft.py
@@ -3,6 +3,7 @@ from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import  QMainWindow
 import serial
 import time
+import matplotlib
 
 import data_form
 
@@ -17,7 +18,8 @@ class Soft(QMainWindow, data_form.Ui_MainWindow):
         self.pushButton.clicked.connect(self.Maj_data)
         self.data = open ("test.csv", "w")
         self.step_time.clicked.connect(self.Maj_time)
-        self.time = 0
+        self.Export.clicked.connect(self.exporter)
+        self.current_time = 0
       
 
 
@@ -31,13 +33,36 @@ class Soft(QMainWindow, data_form.Ui_MainWindow):
         print (self.DAC_SELECTOR.currentIndex(), self.VALUE_INPUT.text())
         vara = str(self.DAC_SELECTOR.currentIndex())
         varb = self.VALUE_INPUT.text()
-        self.data.write(vara  +',' + varb + '\n')
+        self.data.write("data," +vara  +',' + varb + '\n')
 
     def Maj_time (self) :
         print (self.Time_input.text())
-        vart = self.time_scale.currentIndex()
-        print (vart)
+        time_scale = self.time_scale.currentIndex()
+        t_temp = int (self.Time_input.text())
+
+        self.data.write("tempo," + str(time_scale)  +',' + str(t_temp) + '\n')
+
+        if (time_scale == 0):
+            t_temp =t_temp / 1000
+        if (time_scale == 1 ):
+            t_temp = t_temp / 100000
         
+        self.current_time += t_temp
+
+        self.time.setText( "t= " + str(self.current_time))
+        
+
+    def plotter (self, x, y): 
+        print (x , y)
+
+    def exporter (self):
+        self.data.close()
+        self.data = open ("test.csv", "r")
+
+        for rows in self.data :
+            self.plotter( rows[1] , rows [2])
+
+
 
 if __name__ == '__main__':    
     app = QApplication(sys.argv)
